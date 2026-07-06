a0888466ddac1578e0fca02b955692c066e7a32b
Nate Choe <nate@natechoe.dev>
2026-07-06 15:30:08 -0500

    huffman code expansion

--- src/main/java/dev.natechoe.heller/App.java
+++ src/main/java/dev.natechoe.heller/App.java
@@ -4,14 +4,18 @@ import dev.natechoe.crc32.CRC32Engine;
 import java.util.*;
 
 public class App {
+    static void init(int[] l, int f, int e, int b) {
+        for (int i = f; i < e; ++i) l[i] = b;
+    }
     public static void main(String[] args) {
-        Bytes data = new Bytes(new byte[] {65, 66, 67});
-        Bytes deflated = Deflate.literal(data, true);
-        for (byte b: deflated) {
-            System.out.printf("%02x", b);
+        int[] l = new int[288];
+        init(l, 0, 144, 8);
+        init(l, 144, 256, 9);
+        init(l, 256, 280, 7);
+        init(l, 280, 288, 8);
+        String[] b = Deflate.genTree(l);
+        for (int i = 0; i < l.length; ++i) {
+            System.out.printf("%3d: %s\n", i, b[i]);
         }
-        System.out.println();
-        System.out.println(Arrays.toString(CRC32Engine.calculateCRC(
-                        new byte[] { 65, 66, 67 })));
     }
 }
--- src/main/java/dev.natechoe.heller/Deflate.java
+++ src/main/java/dev.natechoe.heller/Deflate.java
@@ -1,5 +1,10 @@
 package dev.natechoe.heller;
 
+import java.util.Map;
+import java.util.TreeMap;
+import java.util.List;
+import java.util.ArrayList;
+
 public class Deflate {
     /* Lx takes 5 bytes, assuming initial byte alignment */
     public static final int CMD_SIZE = 5;
@@ -45,5 +50,125 @@ public class Deflate {
 
     public static Bytes literal(Bytes data) {
         return Deflate.literal(data, false);
+    }
+
+    private static class BitTree {
+        /* literally just a string of ASCII '0' and '1' characters */
+        String data;
+
+        /* the bit data that comes /before/ this node */
+        BitTree prev;
+
+        boolean bfinal;
+
+        BitTree(String data, boolean bfinal, BitTree prev) {
+            this.data = data;
+            this.bfinal = bfinal;
+            this.prev = prev;
+        }
+
+        BitTree(String data, boolean bfinal) {
+            this(data, bfinal, null);
+        }
+
+        BitTree(String data) {
+            this(data, false);
+        }
+
+        Bytes reconstruct() {
+            List<String> fullData = new ArrayList<>();
+            BitTree iter = this;
+            int bfinalIdx = -1;
+            while (iter != null) {
+                if (bfinalIdx == -1 && iter.bfinal) {
+                    bfinalIdx = fullData.size();
+                }
+                fullData.add(iter.data);
+                iter = iter.prev;
+            }
+
+            Bytes ret = new Bytes();
+            byte thisByte = 0;
+            int length = 0;
+            for (int i = fullData.size()-1; i >= 0; --i) {
+                char[] s = fullData.get(i).toCharArray();
+                if (i == bfinalIdx) {
+                    s[0] = 1;
+                }
+                for (char c: s) {
+                    byte b = (byte) (c == '1' ? 1 : 0);
+                    b <<= (length % 8);
+                    thisByte |= b;
+                    ++length;
+
+                    if (length % 8 == 0) {
+                        ret.append(thisByte);
+                    }
+                }
+            }
+
+            if (length % 8 != 0) {
+                ret.append(thisByte);
+            }
+
+            return ret;
+        }
+    }
+
+    /* expresses v as an l bit binary string, including leading zeros and
+     * truncating higher order bits */
+    private static String expand(long v, int l) {
+        char[] d = new char[l];
+        for (int i = l-1; i >= 0; --i) {
+            d[i] = (char) ((v & 1) + '0');
+            v >>= 1;
+        }
+        return new String(d);
+    }
+
+    /* converts from a set of code lengths into a set of huffman codes
+     * assumes that no code is longer than 63 bits */
+    static String[] genTree(int[] lengths) {
+        TreeMap<Integer, List<Integer>> lm = new TreeMap<>();
+        for (int i = 0; i < lengths.length; ++i) {
+            if (!lm.containsKey(lengths[i])) {
+                lm.put(lengths[i], new ArrayList<>());
+            }
+            lm.get(lengths[i]).add(i);
+        }
+
+        long b = 0;
+        int pl = 0;
+        String[] ret = new String[lengths.length];
+        for (Map.Entry<Integer, List<Integer>> lengthClass: lm.entrySet()) {
+            int length = lengthClass.getKey();
+            List<Integer> values = lengthClass.getValue();
+
+            if (pl != 0) {
+                b <<= (length - pl);
+            }
+            pl = length;
+
+            for (int i: values) {
+                String s = expand(b, length);
+                ret[i] = s;
+                ++b;
+            }
+        }
+
+        if (b != (1l << pl)) {
+            return null;
+        }
+        return ret;
+    }
+
+    /**/
+    public static Map<Integer, Bytes> repeat(int length, int offset,
+            boolean bfinal) {
+        return null;
+    }
+
+    public static Map<Integer, Bytes> repeat(int length, int offset) {
+        return Deflate.repeat(length, offset, false);
     }
 }
